6981b0188f91fbf9309ba2ca1f9ce42c98bfa1ce
Gondrak08 <vitor.alecrim.p@gmail.com>
2021-05-26 12:51:24 -0300

    functions cart done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,16 @@
 import react, { useContext } from 'react'
 import { CartContext } from '../contexts/cartContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruck } from '@fortawesome/free-solid-svg-icons'
+
 import CartItem from './CartItem'
 
 export const Cart = () => {
     const context = useContext(CartContext);
+
+    const itemsPrice = context.shoppingCart.reduce((a, c) => a + c.price * c.qty, 0);
+    const shippingPrice = itemsPrice > 0 ? 3.59 : 0;
+    const totalPrice = itemsPrice > 0 ? itemsPrice + shippingPrice: 0;
 
     return (
         <>
@@ -13,6 +20,10 @@ export const Cart = () => {
                     <h2 className="pl-2 pb-2" >Produtos no carrinho de compras</h2>
                     <div>
                         <CartItem/>
+                    </div>
+                    <div className="flex justify-end space-x-3 px-12 mt-2   items-center">
+                        <p>Valor Total: {' '} $ {' '} <span>{totalPrice}</span></p>
+                        <FontAwesomeIcon icon={faTruck} color="red" />
                     </div>
                 </div>
             </div>
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,6 +4,8 @@ import { CartContext } from '../contexts/cartContext'
 export const CartItem = () => {
     const context = useContext(CartContext);
     
+   
+
     return (
         <>
             {context.shoppingCart.map((product) => (
@@ -12,10 +14,10 @@ export const CartItem = () => {
                     <div className="flex-1 ">
                         <h3 className="ml-2" >{product.name}</h3>
                         <div className="w-24 relative top-1  h-auto ml-1  flex px-2 justify-between" >
-                            <button className="button w-6 bg-gray-200" onClick={()=> context.onAdd(product)} >
+                            <button className="butn flex justify-center items-center w-6 bg-gray-200" onClick={()=> context.onAdd(product)} >
                                 +
                             </button>
-                            <button className="btn  w-6 bg-gray-200" onClick={()=> context.onRemove(product)} >
+                            <button className="btn flex justify-center items-center  w-6 bg-gray-200" onClick={()=> context.onRemove(product)} >
                                 -
                             </button>
                         </div>
@@ -23,7 +25,7 @@ export const CartItem = () => {
                     <div>
                         <p>Preço: {product.price} </p>
                         <p>Quantitade: {product.qty}</p>
-                        <p>Total: {} </p>
+                        <p>Total: {product.price * product.qty} </p>
                     </div>
                     <div className="w-20 h-16 order-first">
                         <img src={product.img} alt=""/>
